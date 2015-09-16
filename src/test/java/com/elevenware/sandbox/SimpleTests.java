61db4f4b9ff5f25a1e47fab0517f4bf0be7001d6
George McIntosh <george@elevenware.com>
2015-09-16 15:31:09 +0100

    changing test

--- src/test/java/com/elevenware/sandbox/SimpleTests.java
+++ src/test/java/com/elevenware/sandbox/SimpleTests.java
@@ -2,12 +2,14 @@ package com.elevenware.sandbox;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class SimpleTests {
 
     @Test
     public void test() {
 
-        
+        assertTrue(true);
 
     }
 
