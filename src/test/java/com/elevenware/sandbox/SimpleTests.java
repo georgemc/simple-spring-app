3ba379820dad80820ba65979bc3ef6992fcdb2db
George McIntosh <george@elevenware.com>
2015-09-16 15:36:37 +0100

    change test

--- src/test/java/com/elevenware/sandbox/SimpleTests.java
+++ src/test/java/com/elevenware/sandbox/SimpleTests.java
@@ -2,6 +2,7 @@ package com.elevenware.sandbox;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SimpleTests {
@@ -9,7 +10,7 @@ public class SimpleTests {
     @Test
     public void test() {
 
-        assertTrue(true);
+        assertFalse(!true);
 
     }
 
